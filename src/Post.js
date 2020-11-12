42525a2472117a6fa486f58e6175bcda63d938ba
Karen Tan <ktann.309@gmail.com>
2020-11-12 13:21:59 -0800

    Add buttons and counters to every post component.

--- src/Post.js
+++ src/Post.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Post() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    document.title = `The current counter is ${counter}`;
+  }, [counter]);
+
   return (
-    <div>
-      I may be a Post!
-    </div>
+    <>
+      <div>
+        {counter}
+      </div>
+      <button
+        type="button"
+        onClick={() => { setCounter(counter + 1); }}
+      >
+        Click me!
+      </button>
+    </>
   );
 }
