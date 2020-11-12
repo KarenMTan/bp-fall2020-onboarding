095ac7ac522c6f71d793a14140c4ee320758a20c
Karen Tan <ktann.309@gmail.com>
2020-11-12 13:41:05 -0800

    Submit input results only on button submission.

--- src/Post.js
+++ src/Post.js
@@ -2,12 +2,19 @@ import React, { useState } from 'react';
 
 export default function Post() {
   const [text, setText] = useState('');
+  const [hiddenText, setHiddenText] = useState('');
   return (
     <>
       <div>
         {text}
       </div>
-      <input onChange={(e) => { setText(e.target.value); }} />
+      <input onChange={(e) => { setHiddenText(e.target.value); }} />
+      <button
+        type="button"
+        onClick={() => { setText(hiddenText); }}
+      >
+        Submit
+      </button>
     </>
   );
 }
